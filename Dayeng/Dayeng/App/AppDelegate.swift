f9d37366b8d39c490fb22593260f4b3aaba58e6b
조승기 <beenzino@kookmin.ac.kr>
2023-01-30 16:25:58 +0900

    add: GoogleService-Info.plist gitignore 추가

--- Dayeng/Dayeng/App/AppDelegate.swift
+++ Dayeng/Dayeng/App/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
 
-@main
+
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         sleep(2)
         return true
     }
