7d2f11a6718997c167e9ab5a3ebbc33f78e62708
조승기 <beenzino@kookmin.ac.kr>
2023-01-30 14:21:54 +0900

    add: 프로젝트 생성

---
+++ Dayeng/Dayeng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dayeng
+//
+//  Created by 조승기 on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
