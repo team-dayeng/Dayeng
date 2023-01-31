d9827325f036f6a7f3a331cc11dad51ea5e599b3
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-01-31 15:24:11 +0900

    add: SplashViewController 추가

---
+++ Dayeng/Dayeng/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/01/31.
+//
+
+import UIKit
+import SnapKit
+import Lottie
+
+class SplashViewController: UIViewController {
+    
+}
