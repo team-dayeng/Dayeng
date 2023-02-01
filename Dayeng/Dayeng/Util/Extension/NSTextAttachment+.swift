072cb61cbc0fe4674a6dd423e47f2294fd626b66
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-01 17:12:06 +0900

    feat: NSTextAttachment Image resize 함수 구현

--- Dayeng/Dayeng/Util/Extension/NSTextAttachment+.swift
+++ Dayeng/Dayeng/Util/Extension/NSTextAttachment+.swift
@@ -5,4 +5,13 @@
 //  Created by 배남석 on 2023/02/01.
 //
 
-import Foundation
+import UIKit
+
+extension NSTextAttachment {
+    func setImageHeight(height: CGFloat) {
+        guard let image = image else { return }
+        let ratio = image.size.width / image.size.height
+
+        bounds = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: ratio * height, height: height)
+    }
+}
