5db31dc78687e06ead91529089ce36de31cd1aba
sangyeon <sangyeun33@gmail.com>
2023-02-01 21:40:48 +0900

    feat: UIColor convenience init 추가

---
+++ Dayeng/Dayeng/Util/Extension/UIColor+.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+.swift
+//  Dayeng
+//
+//  Created by  sangyeon on 2023/02/01.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0,
+                  alpha: 1.0)
+    }
+}
