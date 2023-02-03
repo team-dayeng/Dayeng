d929d84338f3f44361dbf9eb069b91a923b66869
sangyeon <sangyeun33@gmail.com>
2023-02-03 17:14:22 +0900

    feat: apple 로그인 설정

--- Dayeng/Dayeng/Presentation/Auth/LoginViewController.swift
+++ Dayeng/Dayeng/Presentation/Auth/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import AuthenticationServices
 
+@available(iOS 13.0, *)     // Apple 로그인은 iOS 13.0 버전 이후부터 지원
 class LoginViewController: UIViewController {
     
     // MARK: - UI properties
@@ -16,14 +17,11 @@ class LoginViewController: UIViewController {
         imageView.image = UIImage(named: "paperBackground")
         return imageView
     }()
-    
     private lazy var logoImage: UIImageView = {
         var imageView: UIImageView = UIImageView()
         imageView.image = UIImage(named: "LogoImage")
         return imageView
     }()
-    
-//    private var appleLoginButton = AuthButton(type: .apple)
     private var appleLoginButton = ASAuthorizationAppleIDButton(
         type: .signIn,
         style: .whiteOutline
@@ -37,6 +35,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        setup()
     }
     
     // MARK: - Helpers
@@ -64,5 +63,57 @@ class LoginViewController: UIViewController {
             $0.top.equalTo(appleLoginButton.snp.bottom).offset(15)
             $0.centerX.height.leading.trailing.equalTo(appleLoginButton)
         }
+    }
+    
+    private func setup() {
+        appleLoginButton.addTarget(
+            self,
+            action: #selector(appleLoginButtonDidTap),
+            for: .touchUpInside)
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    @objc func appleLoginButtonDidTap() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(
+            authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    /// Apple 로그인 텍스트 프로바이딩
+    /// 애플 로그인 버튼을 눌렀을 때 애플 로그인을 modal sheet로 표시해주는 함수
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    /// apple ID 연동 성공시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            // get user info
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            print("user ID: \(userIdentifier)")
+            print("user Name: \(fullName)")
+            print("user email: \(email)")
+        default:
+            break
+        }
+    }
+    
+    /// apple ID 연동 실패시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // TODO: 개발자 계정 승인 후 signing에서 sign in with apple 추가
+        // TODO: error handling
+        print("AppleID Credential failed with error: \(error.localizedDescription)")
     }
 }
