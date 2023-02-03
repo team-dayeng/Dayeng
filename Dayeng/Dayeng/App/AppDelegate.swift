72ac4a9ed29e69dee51bf900a2ff62adfcf5cebb
sangyeon <sangyeun33@gmail.com>
2023-02-03 17:20:25 +0900

    feat: 앱 실행시 apple 로그인 상태 확인

--- Dayeng/Dayeng/App/AppDelegate.swift
+++ Dayeng/Dayeng/App/AppDelegate.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 import FirebaseCore
-
+import AuthenticationServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // 앱 실행시 로그인 상태 확인 (Apple)
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+//        appleIDProvider.getCredentialState(forUserID: /* 로그인 할 userID*/) { (credentialState, error) in
+//            switch credentialState {
+//            case .authorized:
+//                // apple ID credential 유효
+//                print("ID 연동 O")
+//            case .revoked:
+//                // 해당 userID 값이 앱과 연결 취소되어 있으므로 로그인 UI를 표시
+//                print("ID 연동 X")
+//            case .notFound:
+//                // userID가 앱과 연결되어 있지 않으므로 로그인 UI를 표시
+//                print("ID 찾을 수 없음")
+//            default:
+//                break
+//            }
+//        }
+        
         return true
     }
 
