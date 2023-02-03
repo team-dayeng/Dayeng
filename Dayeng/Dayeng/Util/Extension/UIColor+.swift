0fab7fdbd42b7665b06a9cc5e7f5028a83237673
sangyeon <sangyeun33@gmail.com>
2023-02-03 16:26:18 +0900

    feat: hexString으로 UIColor init 함수 추가

--- Dayeng/Dayeng/Util/Extension/UIColor+.swift
+++ Dayeng/Dayeng/Util/Extension/UIColor+.swift
@@ -14,4 +14,13 @@ extension UIColor {
                   blue: CGFloat(blue) / 255.0,
                   alpha: 1.0)
     }
+    
+    convenience init(hexString: String) {
+        let arr = Array(hexString)
+        var rgb: [Int] = []
+        for index in stride(from: 0, to: 5, by: 2) {
+            rgb.append(Int(String(arr[index...(index+1)]), radix: 16)!)
+        }
+        self.init(red: rgb[0], green: rgb[1], blue: rgb[2])
+    }
 }
