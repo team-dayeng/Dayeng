4db8f87ced4c7a9ea87012ca35a7d7713a95ffa0
조승기 <beenzino@kookmin.ac.kr>
2023-02-04 18:32:10 +0900

    design: 페이지 뷰 컨틀롤러 생성

--- Dayeng/Dayeng/Presentation/Main/MainPageViewController.swift
+++ Dayeng/Dayeng/Presentation/Main/MainPageViewController.swift
@@ -5,11 +5,49 @@
 //  Created by 조승기 on 2023/02/04.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import RxSwift
+
+class MainPageViewController: ViewController {
+    // MARK: - UI properties
+    private lazy var pageViewController: UIPageViewController = {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                      navigationOrientation: .horizontal)
+        return pageViewController
+    }()
+    private lazy var leftViewController: CommonMainViewController = {
+        let viewController = MainViewController(viewModel: MainViewModel())
+        viewController.backgroundImage.alpha = 0.5
+        return viewController
+    }()
+    
+    private lazy var centerViewController: CommonMainViewController = {
+        MainViewController(viewModel: MainViewModel())
+    }()
+    
+    private lazy var rightViewController: CommonMainViewController = {
+        MainViewController(viewModel: MainViewModel())
+    }()
+    
+    private lazy var arrayViewControllers: [CommonMainViewController] = {
+        [leftViewController, centerViewController, rightViewController]
+    }()
+    
+    // MARK: - Properties
     var calendarButtonDidTapped: Observable<Void>!
     var resetButtonDidTapped: Observable<Void>!
     
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         setupNaviagationBar()
+        setupViews()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
     private func setupNaviagationBar() {
         let calendarButton = UIBarButtonItem(image: UIImage(systemName: "calendar"),
                                              style: .plain,
