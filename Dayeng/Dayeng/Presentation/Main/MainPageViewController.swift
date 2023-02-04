c44aa59f21c4db2cc6f19573a561af85792de77c
조승기 <beenzino@kookmin.ac.kr>
2023-02-04 18:31:53 +0900

    chore: 네비게이션 설정 메인 -> 페이지 뷰컨트롤러로 이동

--- Dayeng/Dayeng/Presentation/Main/MainPageViewController.swift
+++ Dayeng/Dayeng/Presentation/Main/MainPageViewController.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+    var calendarButtonDidTapped: Observable<Void>!
+    var resetButtonDidTapped: Observable<Void>!
+    
+        setupNaviagationBar()
+    private func setupNaviagationBar() {
+        let calendarButton = UIBarButtonItem(image: UIImage(systemName: "calendar"),
+                                             style: .plain,
+                                             target: nil,
+                                             action: nil)
+        calendarButtonDidTapped = calendarButton.rx.tap.asObservable()
+        
+        let resetButton = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"),
+                                          style: .plain,
+                                          target: nil,
+                                          action: nil)
+        resetButtonDidTapped = resetButton.rx.tap.asObservable()
+        
+        navigationItem.leftBarButtonItem = calendarButton
+        navigationItem.rightBarButtonItem = resetButton
+    }
--- Dayeng/Dayeng/Presentation/Main/MainViewController.swift
+++ Dayeng/Dayeng/Presentation/Main/MainViewController.swift
@@ -32,8 +32,6 @@ class MainViewController: CommonMainViewController {
     
     // MARK: - Properties
     let viewModel: MainViewModel
-    var calendarButtonDidTapped: Observable<Void>!
-    var resetButtonDidTapped: Observable<Void>!
     var backgroundDidTapped: Observable<Void>!
     var answerLabelDidTapped: Observable<Void>!
     
@@ -50,30 +48,12 @@ class MainViewController: CommonMainViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNaviagationBar()
         setupViews()
         configureUI()
         bind()
     }
     
     // MARK: - Helpers
-    private func setupNaviagationBar() {
-        let calendarButton = UIBarButtonItem(image: UIImage(systemName: "calendar"),
-                                             style: .plain,
-                                             target: nil,
-                                             action: nil)
-        calendarButtonDidTapped = calendarButton.rx.tap.asObservable()
-        
-        let resetButton = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"),
-                                          style: .plain,
-                                          target: nil,
-                                          action: nil)
-        resetButtonDidTapped = resetButton.rx.tap.asObservable()
-        
-        navigationItem.leftBarButtonItem = calendarButton
-        navigationItem.rightBarButtonItem = resetButton
-    }
-    
     private func setupViews() {
         [friendButton, settingButton].forEach {
             view.addSubview($0)
