9db73f6ed5435e6f29d7d096c45c6f9ced319b0f
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-06 13:57:46 +0900

    feat: 알람 요일 설정 디자인 구현 및 Cell 선택 이벤트 구현

--- Dayeng/Dayeng/Presentation/Setting/View/AlarmDayCell.swift
+++ Dayeng/Dayeng/Presentation/Setting/View/AlarmDayCell.swift
@@ -6,7 +6,72 @@
 //
 
 import UIKit
+import SnapKit
 
 final class AlarmDayCell: UICollectionViewCell {
+    // MARK: - UI properties
+    private lazy var dayLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        
+        return label
+    }()
     
+    private lazy var isSelectedImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "checkmark")
+        view.contentMode = .scaleAspectFill
+        view.isHidden = true
+        view.tintColor = UIColor(named: "MainColor")
+        
+        return view
+    }()
+    
+    // MARK: - Properties
+    static let identifier: String = "AlarmDayCell"
+    var isSelect: Bool
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        isSelect = false
+        super.init(frame: frame)
+        
+        setupViews()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    private func setupViews() {
+        contentView.addSubview(dayLabel)
+        contentView.addSubview(isSelectedImageView)
+    }
+    
+    private func configureUI() {
+        contentView.backgroundColor = .white
+        
+        dayLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(15)
+            $0.top.bottom.equalToSuperview().inset(5)
+        }
+        
+        isSelectedImageView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(15)
+            $0.top.bottom.equalToSuperview().inset(13)
+        }
+    }
+    
+    func bind(text: String) {
+        dayLabel.text = text
+    }
+    
+    func tappedCell(isSelected: Bool) {
+        isSelectedImageView.isHidden = !isSelected
+        isSelect = isSelected
+    }
 }
+
