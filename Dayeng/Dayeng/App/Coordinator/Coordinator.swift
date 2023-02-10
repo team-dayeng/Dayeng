af98b0559c73110598d1bb3c9e8eadcbb452629a
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-10 19:49:13 +0900

    feat: Coordinator Protocol 추가, AppCoordinator 추가 및 구현

---
+++ Dayeng/Dayeng/App/Coordinator/Coordinator.swift
@@ -0,0 +1,22 @@
+//
+//  Coordinator.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/02/09.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var delegate: CoordinatorDelegate? {get set}
+    
+    init(navigationController: UINavigationController)
+    
+    func start()
+}
+
+protocol CoordinatorDelegate: AnyObject {
+    func didFinished(childCoordinator: Coordinator)
+}
