229a93f9232837a7016267af7234b507bbd0458b
sangyeon <sangyeun33@gmail.com>
2023-02-10 20:02:53 +0900

    add: Question Entity & DTO 구현

---
+++ Dayeng/Dayeng/Data/DTO/QuestionDTO.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionDTO.swift
+//  Dayeng
+//
+//  Created by  sangyeon on 2023/02/09.
+//
+
+import Foundation
+
+struct QuestionDTO: Codable {
+    let english: String
+    let korean: String
+}
