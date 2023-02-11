c0c8af65cceba801106985dd7784cbbd1e2dfe99
조승기 <beenzino@kookmin.ac.kr>
2023-02-11 21:44:34 +0900

    feat: 이벤트 구독

--- Dayeng/Dayeng/Presentation/Setting/ViewModel/AlarmSettingViewModel.swift
+++ Dayeng/Dayeng/Presentation/Setting/ViewModel/AlarmSettingViewModel.swift
@@ -12,13 +12,34 @@ import RxRelay
 final class AlarmSettingViewModel {
     // MARK: - Input
     struct Input {
+        var registButtonDidTapped: Observable<Date>
+        var daysOfWeekDidTapped: Observable<Void>
     }
     // MARK: - Output
     struct Output {
+        var dayList = PublishRelay<String>()
+        var date = PublishRelay<Date>()
     }
     // MARK: - Dependency
+    var disposeBag = DisposeBag()
+    let useCase: AlrarmSettingUseCase
+    // MARK: - LifeCycle
+    init(useCase: AlrarmSettingUseCase) {
+        self.useCase = useCase
+    }
+    
+    // MARK: - Helper
     func transform(input: Input) -> Output {
         let output = Output()
+        input.daysOfWeekDidTapped
+            .subscribe(onNext: {
+                // 코디네이터로 변경
+            }).disposed(by: disposeBag)
+        
+        input.registButtonDidTapped
+            .subscribe(onNext: { date in
+                
+            }).disposed(by: disposeBag)
         return output
     }
 }
