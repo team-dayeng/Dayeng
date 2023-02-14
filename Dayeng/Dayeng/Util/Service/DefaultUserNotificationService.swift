b3c4c6732f5908ecb72d547f3258ff487d17783f
조승기 <beenzino@kookmin.ac.kr>
2023-02-14 14:26:47 +0900

    feat: UserNotifications 서비스 구현 - 권한 묻기, 알림 만들기, 알림 지우기

--- Dayeng/Dayeng/Util/Service/DefaultUserNotificationService.swift
+++ Dayeng/Dayeng/Util/Service/DefaultUserNotificationService.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+    func requestAuthorization() -> Observable<Void> {
+        Observable<Void>.create { observer in
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (allow, error) in
+                if let error = error {
+                    observer.onError(error)
+                }
+                if !allow {
+                    observer.onError(UserNotificationError.notAuthorized)
+                }
+                observer.onNext(())
+            }
+            return Disposables.create()
+        }
+    }
+
+    func createNotification(time: Date, daysOfWeek: [Bool]) -> Observable<Void> {
+        removeAllNotifications()
+        let content = UNMutableNotificationContent()
+        content.title = "일기 쓸 시간"
+        content.body = "ㅆㅓ라"
+        
+        let this = daysOfWeek.enumerated().filter {$0.element}.map { (index, _) in
+            let indexToWeekDay = ((index+1)%7)+1
+            return createNotification(time: time, weekDay: indexToWeekDay, content: content)
+        }
+        
+        return Observable.zip(this)
+            .flatMapLatest { _ in
+                Observable.create { observer in
+                    observer.onNext(())
+                    return Disposables.create()
+                }
+            }
+    }
+    
+    func removeAllNotifications() {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { requests in
+            UNUserNotificationCenter
+                .current()
+                .removePendingNotificationRequests(withIdentifiers: requests.map { $0.identifier })
+        }
+    }
