4593e5cca11e13aa9bdef6302fa2bf4779ce4c51
sangyeon <sangyeun33@gmail.com>
2023-02-21 02:28:18 +0900

    feat: ASAuthorizationControllerProxy 구현

---
+++ Dayeng/Dayeng/ASAuthorizationControllerProxy.swift
@@ -0,0 +1,72 @@
+//
+//  ASAuthorizationControllerProxy.swift
+//  Dayeng
+//
+//  Created by  sangyeon on 2023/02/19.
+//
+
+import AuthenticationServices
+import RxSwift
+import RxCocoa
+
+extension ASAuthorizationController: HasDelegate {
+    public typealias Delegate = ASAuthorizationControllerDelegate
+}
+
+@available(iOS 13.0, *)
+final class ASAuthorizationControllerProxy:
+    DelegateProxy<ASAuthorizationController, ASAuthorizationControllerDelegate>,
+    DelegateProxyType,
+    ASAuthorizationControllerDelegate,
+    ASAuthorizationControllerPresentationContextProviding {
+    
+    var presentationWindow = UIWindow()
+    
+    init(controller: ASAuthorizationController) {
+        super.init(
+            parentObject: controller,
+            delegateProxy: ASAuthorizationControllerProxy.self
+        )
+    }
+    
+    // MARK: - DelegateProxyType
+    
+    static func registerKnownImplementations() {
+        register { authorizationController -> ASAuthorizationControllerProxy in
+            ASAuthorizationControllerProxy(controller: authorizationController)
+        }
+    }
+    
+    // MARK: - Proxy Subject
+    lazy var didComplete = PublishSubject<ASAuthorization>()
+    
+    // MARK: - ASAuthorizationControllerDelegate
+    
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithAuthorization authorization: ASAuthorization
+    ) {
+        didComplete.onNext(authorization)
+        didComplete.onCompleted()
+    }
+    
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithError error: Error
+    ) {
+        didComplete.onError(error)
+    }
+    
+    // MARK: - ASAuthorizationControllerPresentationContextProviding
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        presentationWindow
+    }
+    
+    // MARK: - Completed
+    
+    deinit {
+        didComplete.onCompleted()
+    }
+    
+}
---
+++ Dayeng/Dayeng/AuthenticationServices+Rx.swift
@@ -0,0 +1,34 @@
+//
+//  AuthenticationServices+Rx.swift
+//  Dayeng
+//
+//  Created by  sangyeon on 2023/02/19.
+//
+
+import AuthenticationServices
+import RxSwift
+
+@available(iOS 13.0, *)
+extension Reactive where Base: ASAuthorizationAppleIDProvider {
+    
+    func login(
+        scope: [ASAuthorization.Scope]? = nil,
+        on window: UIWindow,
+        nonce: String?
+    ) -> Observable<ASAuthorization> {
+
+        let request = base.createRequest()
+        request.requestedScopes = scope
+        request.nonce = nonce
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        
+        let proxy = ASAuthorizationControllerProxy.proxy(for: controller)
+        proxy.presentationWindow = window
+        
+        controller.presentationContextProvider = proxy
+        controller.performRequests()
+        
+        return proxy.didComplete
+    }
+}
