84d11252223f2cca6a2f874e0d706ab68c4289fb
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-27 15:01:25 +0900

    add: AddFriendViewModel 생성

---
+++ Dayeng/Dayeng/Presentation/Friend/AddFriendViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AddFriendViewModel.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/02/27.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class AddFriendViewModel {
+    // MARK: - Input
+    
+    // MARK: - Output
+    
+    // MARK: - Dependency
+    
+    // MARK: - LifeCycle
+    
+    // MARK: - Helper
+}
