00a17b9e3c472a83f330d51bf1e69d251e3e139e
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-02-27 15:04:09 +0900

    feat: AddFriendViewModel addButtonDidTapped 바인딩

--- Dayeng/Dayeng/Presentation/Friend/AddFriendViewModel.swift
+++ Dayeng/Dayeng/Presentation/Friend/AddFriendViewModel.swift
@@ -11,12 +11,30 @@ import RxRelay
 
 final class AddFriendViewModel {
     // MARK: - Input
+    struct Input {
+        var addButtonDidTapped: Observable<Void>
+    }
     
     // MARK: - Output
+    struct Output {
+        
+    }
     
     // MARK: - Dependency
+    private let disposeBag = DisposeBag()
     
     // MARK: - LifeCycle
     
     // MARK: - Helper
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        input.addButtonDidTapped
+            .subscribe(onNext: {
+                // Firestore -> User 있는지 확인 -> User -> Friends에 삽입
+            })
+            .disposed(by: disposeBag)
+        
+        return output
+    }
 }
