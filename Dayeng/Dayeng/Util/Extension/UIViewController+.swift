f1666cf39a4a13809f4e25b07210b2c000add9f1
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-03-06 00:57:54 +0900

    feat: UIViewController showToast 함수 추가 및 ToastType Enum 파일 추가

--- Dayeng/Dayeng/Util/Extension/UIViewController+.swift
+++ Dayeng/Dayeng/Util/Extension/UIViewController+.swift
@@ -76,6 +76,27 @@ extension UIViewController {
         }
         present(alertViewController, animated: false)
     }
+    
+    func showToast(type: ToastType) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75,
+                                               y: self.view.frame.size.height-100,
+                                               width: 150,
+                                               height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .systemFont(ofSize: 12)
+        toastLabel.textAlignment = .center
+        toastLabel.text = type.title
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.5, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: { _ in
+            toastLabel.removeFromSuperview()
+        })
+    }
 }
 
 public extension Reactive where Base: UIViewController {
