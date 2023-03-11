da2d382fdb26f35a303d09748cb1d3c2e46e01e7
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-03-11 21:34:59 +0900

    add: LinkBuildService 파일 생성

---
+++ Dayeng/Dayeng/Util/Service/DefaultLinkBuildService.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultLinkBuildService.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/03/11.
+//
+
+import Foundation
+
+final class DefaultLinkBuildService: LinkBuildService {
+    
+}
---
+++ Dayeng/Dayeng/Util/Service/LinkBuildService.swift
@@ -0,0 +1,12 @@
+//
+//  LinkBuildService.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/03/11.
+//
+
+import Foundation
+
+protocol LinkBuildService {
+    
+}
