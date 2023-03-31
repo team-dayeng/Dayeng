559ffdff6f53d60a15e5279cd3884624abde9253
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-03-31 21:02:45 +0900

    add: NetworkDisconnectViewController 파일 생성

---
+++ Dayeng/Dayeng/Presentation/Common/NetworkDisconnectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkDisconnectViewController.swift
+//  Dayeng
+//
+//  Created by 배남석 on 2023/03/31.
+//
+
+import UIKit
+
+final class NetworkDisconnectViewController: UIViewController {
+    // MARK: - UI properties
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    // MARK: - Helpers
+}
