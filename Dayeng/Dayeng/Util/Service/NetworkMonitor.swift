6bb882553ad46cd5fe2157b47c69cb0efb152fa5
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2023-03-31 21:00:22 +0900

    feat: NetworkMonitor 모니터링 Start, Stop 메서드 구현

--- Dayeng/Dayeng/Util/Service/NetworkMonitor.swift
+++ Dayeng/Dayeng/Util/Service/NetworkMonitor.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import Network
 
 final class NetworkMonitor {
+    static let shared = NetworkMonitor()
+    private let monitor: NWPathMonitor
+    var isConnected: Bool = false
     
+    init() {
+        self.monitor = NWPathMonitor()
+    }
+    
+    func startMonitoring(statusHandler: @escaping (NWPath.Status) -> Void) {
+        self.monitor.start(queue: DispatchQueue.global())
+        self.monitor.pathUpdateHandler = { path in
+            DispatchQueue.main.async {
+                self.isConnected = path.status == .satisfied ? true : false
+                statusHandler(path.status)
+            }
+        }
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
 }
