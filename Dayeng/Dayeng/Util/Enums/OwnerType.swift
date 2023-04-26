34d868e361554e74a2b3418f37a0eb8a4301ec33
조승기 <beenzino@kookmin.ac.kr>
2023-04-26 21:42:32 +0900

    feat: OnwerType - if문 사용을 위해 Equtable 추가

--- Dayeng/Dayeng/Util/Enums/OwnerType.swift
+++ Dayeng/Dayeng/Util/Enums/OwnerType.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
-enum OwnerType {
+enum OwnerType: Equatable {
     case mine
     case friend(user: User)
+    
+    
+    static func == (lhs: OwnerType, rhs: OwnerType) -> Bool {
+        switch (lhs, rhs) {
+        case (.mine, .mine):
+            return true
+        case (.friend, .friend):
+            return true
+        default:
+            return false
+        }
+    }
 }
